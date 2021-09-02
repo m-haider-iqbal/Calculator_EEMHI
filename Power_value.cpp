9832cfd4246589005d52b26ca3d0e7a9f33d450f
EE M Haider Iqbal <63361394+mhaideriqbal@users.noreply.github.com>
2021-09-02 17:18:16 +0500

    Create Power_value.cpp

---
+++ Power_value.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int a ;
+int b ;
+double res;
+
+
+int main()
+{
+    cout<<"Please Enter the Value of a & b\n";
+    cin>>a;
+    res = pow(a,3);
+    cout<<res;
+
+    return 0;
+}
