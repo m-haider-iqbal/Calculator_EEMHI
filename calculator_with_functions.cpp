15c9c700bbfcbfa4137598fc47ce872ac89a281f
EE M Haider Iqbal <63361394+mhaideriqbal@users.noreply.github.com>
2021-09-02 16:34:38 +0500

    Update calculator_with_functions.cpp

--- calculator_with_functions.cpp
+++ calculator_with_functions.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
+//#include<conio.h>
 #include<math.h>
+//#include<stdlib.h>
+//#include<iomanip>
 
     float num1, num2, res;
     int Select;
     
 using namespace std;
 
+void sum();
+void min();
+void mul();
+void divv();
+
+int main ()
+{
+            cout<<"\nEnter Any Two Numbers: \n";
+            cin>>num1>>num2;
+            sum();
+            min();
+            mul();
+            divv();
+
+//   cout<<endl;
+
+    return 0;
+    
+}
 
 void sum()
 {
@@ -37,18 +59,4 @@ void divv()
         cout<<"\nYou Entered No2 = "<<num2;
         res = num1/num2;
         cout<<"\n\nResult = "<<res;
-}
-
-    
-int main ()
-{
-            cout<<"\nEnter Any Two Numbers: \n";
-            cin>>num1>>num2;
-            sum();
-            min();
-            mul();
-            divv();
-
-    return 0;
-    
 }
